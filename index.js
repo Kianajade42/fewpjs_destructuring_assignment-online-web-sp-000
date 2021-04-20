baa60cdcb6703b7b3bfe28dbf3cddb70007e494d
Kiana Long <kianajade42.kl@gmail.com>
2021-04-20 15:10:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,4 +47,3 @@ const [ , , , , , indg, ]=colors
 const {muppetName, color, song, job, partner}=muppet
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 const { song2, song4} = nestedMuppet.album.the_muppet_movie
-const { k_color, k_job, k_partner } = k_muppet
